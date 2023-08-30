5f70c2ef499aad7084708810d355ca44cf376ffd
ntritin62 <21522677@gm.uit.edu.vn>
2023-08-31 00:21:18 +0700

    finish section-2-30

---
+++ section-2-3/app.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: '',
+    };
+  },
+  computed: {
+    fullname() {
+      console.log('running again...');
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'tri tin';
+    },
+  },
+  methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+    },
+    resetInput() {
+      this.name = '';
+    },
+  },
+});
+
+app.mount('#events');
