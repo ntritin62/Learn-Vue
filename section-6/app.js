0685b36dd4172f6e415d3feb2bced5803b852608
ntritin62 <21522677@gm.uit.edu.vn>
2023-09-03 15:39:06 +0700

    finished section-6-71

--- section-6/app.js
+++ section-6/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailAreVisible: false,
       friends: [
         {
           id: 'manuel',
@@ -18,9 +17,35 @@ const app = Vue.createApp({
       ],
     };
   },
+});
+
+app.component('friend-contact', {
+  template: `
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails()">
+        {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+      </button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+    </li>
+    `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenzo',
+        phone: '01234 5678 991',
+        email: 'manuel@localhost.com',
+      },
+    };
+  },
   methods: {
     toggleDetails() {
-      this.detailsAreVisible = !this.detailAreVisible;
+      this.detailsAreVisible = !this.detailsAreVisible;
     },
   },
 });
