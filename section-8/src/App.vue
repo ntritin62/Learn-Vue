bf527b625a70b5d33d2144b575309301256af371
ntritin62 <21522677@gm.uit.edu.vn>
2023-09-04 16:10:53 +0700

    finish section-8-94

--- section-8/src/App.vue
+++ section-8/src/App.vue
@@ -14,7 +14,6 @@
         name="Julie Jones"
         phone-number="0987 654421 21"
         email-address="julie@localhost.com"
-        is-favorite="0"
       ></friend-contact>
     </ul>
   </section>
