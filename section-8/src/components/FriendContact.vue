cc426d6c05f0446448d51401d242ee1da69da6e8
ntritin62 <21522677@gm.uit.edu.vn>
2023-09-04 19:58:16 +0700

    finish section-8-101

--- section-8/src/components/FriendContact.vue
+++ section-8/src/components/FriendContact.vue
@@ -15,6 +15,7 @@
         {{ emailAddress }}
       </li>
     </ul>
+    <button @click="$emit('delete', id)">Delete</button>
   </li>
 </template>
 
@@ -47,17 +48,17 @@ export default {
       // }
     },
   },
-  // emits: ['toggle-favorite'],
-  emits: {
-    'toggle-favorite': function (id) {
-      if (id) {
-        return true;
-      } else {
-        console.warn('id is missing');
-        return false;
-      }
-    },
-  },
+  emits: ['toggle-favorite', 'delete'],
+  // emits: {
+  //   'toggle-favorite': function (id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn('id is missing');
+  //       return false;
+  //     }
+  //   },
+  // },
   data() {
     return {
       detailsAreVisible: false,
@@ -69,6 +70,9 @@ export default {
     },
     toggleFavorite() {
       this.$emit('toggle-favorite', this.id);
+    },
+    deleteFriend() {
+      this.$emit('delete');
     },
   },
 };
