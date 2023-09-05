3980dfae6a9da3e241c77d50ab96fc15e7c06f2f
ntritin62 <21522677@gm.uit.edu.vn>
2023-09-05 20:04:25 +0700

    finish section-9-115

--- section-9/src/App.vue
+++ section-9/src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <div>
     <the-header></the-header>
+    <!-- <TheHeader /> -->
     <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps.anotherProp }}</p>
+    </course-goals>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import UserInfo from './components/UserInfo.vue';
 import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
+
 export default {
   components: {
     TheHeader,
-    UserInfo,
     BadgeList,
+    UserInfo,
+    CourseGoals,
   },
   data() {
     return {
