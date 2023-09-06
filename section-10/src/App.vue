31c184f0c7905bc02dd9f4978debcc314790af39
ntritin62 <21522677@gm.uit.edu.vn>
2023-09-06 23:42:03 +0700

    finish section-10-133

--- section-10/src/App.vue
+++ section-10/src/App.vue
@@ -1,30 +1,15 @@
 <template>
   <the-header title="RememberMe"></the-header>
+  <the-resources></the-resources>
 </template>
 
 <script>
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/LearningResource/TheResources.vue';
 export default {
   components: {
     TheHeader,
-  },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google ...',
-          link: 'https://google.com',
-        },
-      ],
-    };
+    TheResources,
   },
 };
 </script>
