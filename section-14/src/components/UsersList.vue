ec31fb9518cac61d513af7887db8c4a7f60fd0e0
ntritin62 <21522677@gm.uit.edu.vn>
2023-09-12 23:55:06 +0700

    finish section-14-206

--- section-14/src/components/UsersList.vue
+++ section-14/src/components/UsersList.vue
@@ -23,8 +23,7 @@ export default {
       this.users.unshift(enteredUserName);
     },
     removeUser(user) {
-      const userIndex = this.users.indexOf(user);
-      this.users.splice(userIndex, 1);
+      this.users = this.users.filter((usr) => usr !== user);
     },
   },
 };
@@ -59,6 +58,7 @@ li {
 
 .user-list-leave-active {
   transition: all 1s ease-in;
+  position: absolute;
 }
 
 .user-list-leave-to {
