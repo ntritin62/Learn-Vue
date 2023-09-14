72be801dc5d8d961f853713a402248ec11cbb1f7
ntritin62 <21522677@gm.uit.edu.vn>
2023-09-14 18:57:33 +0700

    finish section-15-217

--- section-15/src/App.vue
+++ section-15/src/App.vue
@@ -24,7 +24,7 @@ export default {
   methods: {
     addOne() {
       // this.$store.commit('increase', { value: 10 });
-      this.$store.commit({ type: 'increase', value: 10 });
+      this.$store.dispatch({ type: 'increase', value: 10 });
     },
   },
 };
