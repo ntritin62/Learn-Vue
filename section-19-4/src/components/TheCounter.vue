11e85cdc09a18d81f0e37f70951b2f5b76478221
ntritin62 <21522677@gm.uit.edu.vn>
2023-09-22 21:21:41 +0700

    finished section-19-305

---
+++ section-19-4/src/components/TheCounter.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>{{ counter }}</h2>
+</template>
+
+<script>
+import { computed } from 'vuex';
+import { useStore } from 'vuex';
+export default {
+  setup() {
+    const store = useStore();
+
+    const counter = computed(() => {
+      return store.getters.counter;
+    });
+
+    return { counter };
+  },
+};
+</script>
