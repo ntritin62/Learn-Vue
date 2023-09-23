6b6e6a43186642631091d08d6f8ff3e32fceb7d4
ntritin62 <21522677@gm.uit.edu.vn>
2023-09-23 22:55:16 +0700

    finished section-20-314

---
+++ section-20-2/src/hooks/alert.js
@@ -0,0 +1,17 @@
+import { ref } from 'vue';
+export default function useAlert() {
+  const alertIsVisible = ref(false);
+
+  function showAlert() {
+    alertIsVisible.value = true;
+  }
+  function hideAlert() {
+    alertIsVisible.value = false;
+  }
+
+  return {
+    alertIsVisible,
+    showAlert,
+    hideAlert,
+  };
+}
