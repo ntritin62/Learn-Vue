4ac4d426feb25aa5bd9bbca9309ccda7458ed335
ntritin62 <21522677@gm.uit.edu.vn>
2023-09-24 00:28:27 +0700

    finished section-20-318

--- section-20-3/src/components/users/UserList.vue
+++ section-20-3/src/components/users/UserList.vue
@@ -26,7 +26,7 @@
 </template>
 
 <script>
-import { ref, computed } from 'vue';
+import { ref, computed, toRefs } from 'vue';
 
 import UserItem from './UserItem.vue';
 import useSearch from '../../hooks/search.js';
@@ -37,35 +37,10 @@ export default {
   props: ['users'],
   emits: ['list-projects'],
   setup(props) {
-    // const enteredSearchTerm = ref('');
-    // const activeSearchTerm = ref('');
-
-    // const availableUsers = computed(function () {
-    //   let users = [];
-    //   if (activeSearchTerm.value) {
-    //     users = props.users.filter((usr) =>
-    //       usr.fullName.includes(activeSearchTerm.value)
-    //     );
-    //   } else if (props.users) {
-    //     users = props.users;
-    //   }
-    //   return users;
-    // });
-
-    // watch(enteredSearchTerm, function (newValue) {
-    //   setTimeout(() => {
-    //     if (newValue === enteredSearchTerm.value) {
-    //       activeSearchTerm.value = newValue;
-    //     }
-    //   }, 300);
-    // });
-
-    // function updateSearch(val) {
-    //   enteredSearchTerm.value = val;
-    // }
+    const { users } = toRefs(props);
 
     const [enteredSearchTerm, availableItems, updateSearch] = useSearch(
-      props.users,
+      users,
       'fullName'
     );
 
