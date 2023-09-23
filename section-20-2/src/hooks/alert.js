4b24b1928100964763855d3c05622f5beb39417f
ntritin62 <21522677@gm.uit.edu.vn>
2023-09-23 23:06:26 +0700

    finished section-20-315

--- section-20-2/src/hooks/alert.js
+++ section-20-2/src/hooks/alert.js
@@ -1,6 +1,6 @@
 import { ref } from 'vue';
-export default function useAlert() {
-  const alertIsVisible = ref(false);
+export default function useAlert(startingVisibility = false) {
+  const alertIsVisible = ref(startingVisibility);
 
   function showAlert() {
     alertIsVisible.value = true;
